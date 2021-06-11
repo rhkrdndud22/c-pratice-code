16bb3f26f5d9ffdf009423eb7a96207a679b0b44
WooYoung <77717717+rhkrdndud22@users.noreply.github.com>
2021-06-11 15:37:01 +0900

    싱글톤

---
+++ 0611 pratice/0611 pratice/SingelTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0611_pratice
+{
+    class SingelTest
+    {
+        private static SingelTest inst;
+        private int data;
+        
+        
+        
+        public SingelTest(Random r)
+        {
+            data = r.Next(1, 100);
+        }
+
+        public int getData()
+        {
+            return data;
+        }
+
+        public static SingelTest getinstance(Random r)
+        {
+            if (inst == null)
+            {
+                inst = new SingelTest(r);
+            }
+            return inst;
+        }
+
+
+    }
+}
---
+++ 0611 pratice/0611 pratice/singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0611_pratice
+{
+    class sinqgleton
+    {
+        static void Main(string[] args)
+        {
+             Random r = new Random();
+
+
+            for (int i=0; i<5; i++)
+            {
+                SingelTest st1 = new SingelTest(r);
+                Console.WriteLine("인스턴스:"+st1.getData());
+
+
+                Console.WriteLine("싱글 톤:" + SingelTest.getinstance(r).getData());
+            }
+
+           
+
+
+        }
+
+
+    }
+}
