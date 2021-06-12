3867f275f54471b0ffe22873c660b05af34bb5da
WooYoung <77717717+rhkrdndud22@users.noreply.github.com>
2021-06-12 17:51:32 +0900

    선택정렬

---
+++ Sort정렬.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratice_0612
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+         
+            int[] arr = new int[50];
+            for(int i=0; i<arr.Length; i++)
+            {
+                int a = r.Next(1, 50);
+                arr[i] += a;
+               
+
+                
+            }
+
+
+            Sort(arr);
+            
+
+         
+
+           
+
+
+        }
+
+        
+        public static void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+
+                        int temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+
+
+                        
+                    }
+                    
+                }
+                Console.WriteLine(arr[i]);
+
+
+            }
+        }
+    }
+}
